9bca74186b412eeae288cd9756c32247b0217075
Ryan Breidenbach <ryan.breidenbach+github@gmail.com>
2011-05-15 21:43:12 -0500

    First passing test.

--- src/test/java/com/twoqubed/sjug/spel/SpelBeanTest.java
+++ src/test/java/com/twoqubed/sjug/spel/SpelBeanTest.java
@@ -1,10 +1,14 @@
 package com.twoqubed.sjug.spel;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
 
 public class SpelBeanTest {
 
@@ -23,5 +27,10 @@ public class SpelBeanTest {
     @Test
     public void testSystemProperty() {
         System.setProperty("my.property", "foo");
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spel-context.xml");
+        SpelBean spelBean = context.getBean("spelBean", SpelBean.class);
+
+        assertEquals("foo", spelBean.getMyProperty());
     }
 }
