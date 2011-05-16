5b14ba984dda3bd8d75807513d1b4a919bef19ca
Ryan Breidenbach <ryan.breidenbach+github@gmail.com>
2011-05-15 22:32:36 -0500

    More tests.

--- src/test/java/com/twoqubed/sjug/spel/SpelTest.java
+++ src/test/java/com/twoqubed/sjug/spel/SpelTest.java
@@ -34,10 +34,35 @@ public class SpelTest {
         bean.setName("foo");
         context.setRootObject(bean);
         
-
         Expression exp = parser.parseExpression("name");
         String name= (String) exp.getValue(bean);
 
         assertEquals("foo", name);
+    }
+
+    @Test
+    public void testParseMathmaticalComparison() {
+        assertTrue(parser.parseExpression("2 == 2").getValue(Boolean.class));
+        assertTrue(parser.parseExpression("3 > 2").getValue(Boolean.class));
+    }
+
+    @Test
+    public void testParseMathmaticalOperations() {
+        Integer value = parser.parseExpression("1 + 1").getValue(Integer.class);
+        assertEquals(new Integer(2), value);
+
+        value = parser.parseExpression("5 % 2").getValue(Integer.class);
+        assertEquals(new Integer(1), value);
+    }
+
+    @Test
+    public void testPropertyAssignment() {
+        SpelBean bean = new SpelBean();
+        context.setRootObject(bean);
+
+        Expression exp = parser.parseExpression("name");
+        exp.setValue(bean, "foo");
+
+        assertEquals("foo", bean.getName());
     }
 }
